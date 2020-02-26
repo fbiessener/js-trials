a77cec3de609d11e9ac18a24db7026073e1e2484
Your Name <you@example.com>
2020-02-26 09:11:44 -0800

    OG + printIndices

---
+++ arrays.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+  // for...in loop will enumerate for items that have Enumerable property
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+  for (const i in items) {
+    console.log(`${items[i]}: ${i}`);
+  }
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
