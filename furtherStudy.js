02cd6182750f43baa0783cce5fe1407cac319257
Your Name <you@example.com>
2020-02-26 14:37:06 -0800

    wordsInCommon in further study completed

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,8 +1,16 @@
 "use strict";
 
+const words1 = new Set(['apple', 'cherry', 'berry', 'banana'])
+const words2 = new Set(['apple', 'cherry', 'berry'])
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  let intersectionSet = new Set();
+
+  for (const word of words1) {
+    if (words2.has(word)) {
+      intersectionSet.add(word)
+    }
+  } return console.log(intersectionSet)
 }
 
 
