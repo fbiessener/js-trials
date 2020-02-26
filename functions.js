57c73b66e439c34fe7d96ef78fef8d771c0fb786
Your Name <you@example.com>
2020-02-26 13:03:13 -0800

    functions.js complete

--- functions.js
+++ functions.js
@@ -1,16 +1,36 @@
 "use strict";
 
+const HOMETOWN = "Brooklyn Park"
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+  if (town === HOMETOWN) {
+    return true;
+  } return false;
+}
 
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(fname, lname) {
+  return (`${fname} ${lname}`);
+}
 
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax = 0.05) {
+  const subTotal = (basePrice * (1 + tax));
+  let fee = 0
+
+  if (state === 'CA') {
+    fee = (0.03 * subTotal);
+  } else if (state === 'PA') {
+    fee = 2;
+  } else if (state === 'MA') {
+    if (basePrice <= 100) {
+      fee = 1;
+    } fee = 3;
+  } return subTotal + fee;
+}
